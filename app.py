d98cc422147162b51435c76ef37ac6c9be425a86
David Sanders <dsanderscanada@gmail.com>
2017-05-05 13:08:39 -0400

    Implement get_game method

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def play(game):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.DEBUG)
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
     setup()
