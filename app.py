fbb89ad2a9dfe90ec72777e8cc39ec3741605117
David Sanders <dsanderscanada@gmail.com>
2017-05-05 12:34:13 -0400

    Update object oriented approach for console game

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ def setup():
 
 
 def play(game):
-    game.check_server_ready()
-    print("Sorry, the cowbull game isn't available right now; "
-          "please come back later. The issue has been logged.")
+    ready = game.check_server_ready()
+    if ready:
+        print("Coming soon")
+    else:
+        print("Sorry, the cowbull game isn't available right now; "
+              "please come back later. The issue has been logged.")
 
 
 if __name__ == "__main__":
