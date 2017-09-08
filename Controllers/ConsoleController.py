7fae9106597ae3636d12a9d91d1953bcb82c74f9
David Sanders <dsanderscanada@gmail.com>
2017-09-08 13:47:55 -0400

    Update status reporting

--- Controllers/ConsoleController.py
+++ Controllers/ConsoleController.py
@@ -57,6 +57,7 @@ class ConsoleController(AbstractController):
                     # returned by the Game model needs to be delivered to the
                     # user. The finish_message is updated and the loop is broken.
                     self.io_controller.draw_screen(current_try=counter)
+                    self.io_controller.report_status(message=output["outcome"]["status"])
                     self.io_controller.finish(finish_message=output["outcome"]["status"])
                     break
                 elif return_signal == self.SIGNAL_ERROR:
